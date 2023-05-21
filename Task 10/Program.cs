657cdcf67a0f7b94a8d61b1b647c301ba8ae75c7
Aleksandra <korzun1999@gmail.com>
2023-05-21 20:09:33 +0600

    first commit

---
+++ Task 10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x<1000 && x>99)
+{
+    int two = x / 10;
+    int result = two % 10;
+    Console.Write(result);
+}
+else
+Console.WriteLine("Ввели не трехзначное число");
---
+++ Task 6/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) Console.WriteLine("Число {0} четное",number);
+else Console.WriteLine("Число {0} не четное",number);
