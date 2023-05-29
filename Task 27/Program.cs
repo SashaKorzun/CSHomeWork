a11549bd6afac036eabd3bdbfabad4d374a26fdf
Aleksandra <korzun1999@gmail.com>
2023-05-29 17:43:08 +0600

    29-2

---
+++ Task 27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+System.Console.WriteLine();
+
+int number = new Random().Next(1, 100);
+int sumDigit = 0;
+System.Console.Write($"Сумма цифр числа {number} равна ");
+
+while (number > 0)
+{
+    int digit = number % 10;
+    sumDigit = sumDigit + digit;
+    number = number / 10;
+}
+
+System.Console.WriteLine(sumDigit);
+System.Console.WriteLine();
